7475bd83e9cb92c50c409ad99d5ac3458090d925
Glitch (hello-express) <none>
2022-07-12 03:18:18 +0000

    🐚🌝 Checkpoint ./index.js:22320980/40 ./package.json:22320980/1894

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { ShardingManager } = require('discord.js');
-const manager = new ShardingManager('./bot.js', { token: 'BOTUNUZUN TOKENİ', totalShards: 2  });
+const manager = new ShardingManager('./bot.js', { token: '"discord.js": "^12.5.3",', totalShards: 2  });
 
 manager.spawn();
 manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}\nWorking....`));
